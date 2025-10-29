73b50341a75606b8402b1ca8b4321b173377480a
pedromartins680 <pedromartins680@gmail.com>
2025-10-29 12:05:24 +0000

    e

---
+++ FichasPraticas/src/Ficha02/Ex04.java
@@ -0,0 +1,20 @@
+package Ficha02;
+
+import java.util.Scanner;
+
+public class Ex04 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int posicao;
+
+        System.out.println("Quantos pontos ganhaste dependendo da tua posição");
+
+        System.out.print("Qual foi a sua posição?");
+        posicao = input.nextInt();
+
+        if ()
+    }
+}

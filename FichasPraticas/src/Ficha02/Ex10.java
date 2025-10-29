06ee8e17920446316a8e5c90533b6fe26612f5b1
pedromartins680 <pedromartins680@gmail.com>
2025-10-29 23:36:22 +0000

    10

---
+++ FichasPraticas/src/Ficha02/Ex10.java
@@ -0,0 +1,57 @@
+package Ficha02;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Realize a operação desejada entre 2 números");
+
+        double valor1, valor2, resultado;
+        String operacao;
+
+        System.out.print("Introduza o primeiro número:");
+        valor1 = input.nextDouble();
+
+        System.out.print("Introduza a operação aritmética pretendida(+, -, /, *, %):");
+        operacao = input.next();
+        
+        System.out.print("Introduza o segundo número:");
+        valor2 = input.nextDouble();
+
+
+
+        switch (operacao) {
+
+            case "+":
+                resultado = valor1 + valor2;
+                System.out.print("O resultado é: " + resultado);
+                break;
+
+            case "-":
+                resultado = valor1 - valor2;
+                System.out.print("O resultado é: " + resultado);
+                break;
+
+            case "*":
+                resultado = valor1 * valor2;
+                System.out.print("O resultado é: " + resultado);
+                break;
+
+            case "/":
+                resultado = valor1 / valor2;
+                System.out.print("O resultado é: " + resultado);
+                break;
+
+            case "%":
+                resultado = valor1 % valor2;
+                System.out.print("O resultado é: " + resultado);
+                break;
+
+        }
+
+    }
+}

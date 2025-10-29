aace7f854c3a41c516ff2c5cb13671e9788be76f
pedromartins680 <pedromartins680@gmail.com>
2025-10-29 11:20:30 +0000

    a

--- FichasPraticas/src/Ficha02/Ex02.java
+++ FichasPraticas/src/Ficha02/Ex02.java
@@ -15,5 +15,14 @@ public class Ex02 {
         System.out.print("Introduza o seu salário bruto:");
         salariobruto = input.nextDouble();
 
+        if (salariobruto <= 15000) {
+            salarioliquido = salariobruto * 0.8;
+            System.out.print("O salário liquido é: " + salarioliquido);
+        }
+        else {
+            salarioliquido = salariobruto * 0.7;
+            System.out.print("O Salário liquido é:" + salarioliquido);
+        }
+
     }
 }
